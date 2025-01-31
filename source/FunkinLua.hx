96ad77c3449e9f2b40d77a63dbcf0cb3cd131914
Dokiazi <cloudsbr65@gmail.com>
2025-01-31 20:31:34 -0300

    Updated .hx
    Discord.hx, FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -159,7 +159,7 @@ class FunkinLua {
 		set('rating', 0);
 		set('ratingName', '');
 		set('ratingFC', '');
-		set('version', MainMenuState.Version.trim());
+		set('version', MainMenuState.version.trim());
 
 		set('inGameOver', false);
 		set('mustHitSection', false);
