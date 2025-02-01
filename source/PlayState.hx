edab21b90dc8f39b088b685bb36b7fc96c17a248
Dokiazi <cloudsbr65@gmail.com>
2025-02-01 13:29:59 -0300

    Update PlayState.hx
    Red and green health bar :D

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1475,8 +1475,7 @@ class PlayState extends MusicBeatState
 	}
 
 	public function reloadHealthBarColors() {
-		healthBar.createFilledBar(FlxColor.fromRGB(dad.healthColorArray[0], dad.healthColorArray[1], dad.healthColorArray[2]),
-			FlxColor.fromRGB(boyfriend.healthColorArray[0], boyfriend.healthColorArray[1], boyfriend.healthColorArray[2]));
+		healthBar.createFilledBar(0xFFFF0000, 0xFF66FF33);
 
 		healthBar.updateBar();
 	}
