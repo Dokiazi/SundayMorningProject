b01413bf06846b50bfb160e4fe279daa16122210
Dokiazi <cloudsbr65@gmail.com>
2025-02-01 10:54:46 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1475,11 +1475,11 @@ class PlayState extends MusicBeatState
 		#end
 	}
 
-	public function reloadHealthBarColors() {
-		healthBar.createFilledBar(FlxColor.fromRGB(dad.healthColorArray[0], dad.healthColorArray[1], dad.healthColorArray[2]),
-			FlxColor.fromRGB(boyfriend.healthColorArray[0], boyfriend.healthColorArray[1], boyfriend.healthColorArray[2]));
+	//public function reloadHealthBarColors() {
+	//	healthBar.createFilledBar(FlxColor.fromRGB(dad.healthColorArray[0], dad.healthColorArray[1], dad.healthColorArray[2]),
+	//		FlxColor.fromRGB(boyfriend.healthColorArray[0], boyfriend.healthColorArray[1], boyfriend.healthColorArray[2]));
 
-		healthBar.updateBar();
+	//	healthBar.updateBar();
 	}
 
 	public function addCharacterToList(newCharacter:String, type:Int) {
